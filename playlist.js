874039d8b3ab3448f6eae875799ac13a33278308
FrankieMarie <frankiemarie83@gmail.com>
2018-03-09 14:24:39 -0700

    activate clear tracks button

--- playlist.js
+++ playlist.js
@@ -1,19 +1,22 @@
 let allAlbums = document.getElementById('albums')
 let bin = document.getElementById('bin')
+let clearBin = document.getElementById('clear_bin')
+let submitBin = document.getElementById('submit_bin')
 
 fetch('https://lit-fortress-6467.herokuapp.com/object')
 .then((response) => response.json())
 .then((data) => {
     let albums = data.results
     albums.forEach(element => {
-        //let imageDiv = document.createElement('div')
         let image = document.createElement('img')
         image.src = './images/' + element.cover_art
         image.addEventListener('click', function(){
             let output = `${element.artist}: ${element.title} </br>`
             bin.innerHTML += output
         })
-        //imageDiv.appendChild(image).classList.add('albumImg')
         allAlbums.appendChild(image).classList.add('albumImg')
     });
+    clearBin.addEventListener('click', function(){
+        bin.innerHTML = ''
+    })
 })
