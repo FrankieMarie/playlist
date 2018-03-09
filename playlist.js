d496aed97b1432266ea3db407b8f6aae7646f856
FrankieMarie <frankiemarie83@gmail.com>
2018-03-09 15:24:01 -0700

    started post request

--- playlist.js
+++ playlist.js
@@ -2,6 +2,7 @@ let allAlbums = document.getElementById('albums')
 let bin = document.getElementById('bin')
 let clearBin = document.getElementById('clear_bin')
 let submitBin = document.getElementById('submit_bin')
+//let binContents = []
 
 fetch('https://lit-fortress-6467.herokuapp.com/object')
 .then((response) => response.json())
@@ -20,3 +21,18 @@ fetch('https://lit-fortress-6467.herokuapp.com/object')
         bin.innerHTML = ''
     })
 })
+
+
+// submitBin.addEventListener('submit', function(e){
+//     e.preventDefault()
+//     fetch('https://lit-fortress-6467.herokuapp.com/post', {
+//         method: 'POST',
+//         headers: {
+//             'Accept': 'application/json, text/plain, */*',
+//             'Content-Type': 'application/json'
+//           },
+//         body: {}
+//     })
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+// })
