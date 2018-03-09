88a76caa813b2bf73f13bce6ca38bf6e90dd4fd1
FrankieMarie <frankiemarie83@gmail.com>
2018-03-09 13:45:38 -0700

    album info into bin on click

--- playlist.js
+++ playlist.js
@@ -1,20 +1,19 @@
 let allAlbums = document.getElementById('albums')
+let bin = document.getElementById('bin')
 
 fetch('https://lit-fortress-6467.herokuapp.com/object')
 .then((response) => response.json())
 .then((data) => {
     let albums = data.results
     albums.forEach(element => {
-        let imageDiv = document.createElement('div')
+        //let imageDiv = document.createElement('div')
         let image = document.createElement('img')
         image.src = './images/' + element.cover_art
-        imageDiv.appendChild(image).classList.add('albumImg')
-        allAlbums.appendChild(imageDiv).classList.add('albumImg')
+        image.addEventListener('click', function(){
+            let output = `${element.artist}: ${element.title}`
+            bin.innerHTML += output
+        })
+        //imageDiv.appendChild(image).classList.add('albumImg')
+        allAlbums.appendChild(image).classList.add('albumImg')
     });
 })
-
-// allAlbums.addEventListener('click', addToBin)
-
-// function addToBin(e){
-    
-// }
