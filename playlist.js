042f533091250df817d8e0e44e2adf71417ddf63
FrankieMarie <frankiemarie83@gmail.com>
2018-03-09 09:20:42 -0700

    album images appear on playlist page

--- playlist.js
+++ playlist.js
@@ -1,6 +1,14 @@
+let allAlbums = document.getElementById('albums')
+
 fetch('https://lit-fortress-6467.herokuapp.com/object')
 .then((response) => response.json())
 .then((data) => {
     let albums = data.results
     console.log(albums)
+    albums.forEach(element => {
+        let image = document.createElement('img')
+        image.src = './images/' + element.cover_art
+        allAlbums.appendChild(image).classList.add('albumDivs')
+    });
+    console.log(allAlbums)
 })
